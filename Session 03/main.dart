10d020494e40ed557f60aecb51ab7d75b9824fe0
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-03-04 23:36:20 +0530

    create main.dart

---
+++ Session 03/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 25;
+  String city = 'Matale';
+  bool isProfileLocked = false;
+  double weight = 60.5;
+  dynamic other = 'Other';
+
+  print(age);
+  print(city);
+  
+}
