927b1449ae8d875c964923244096fa26ac1f638e
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-03-05 00:48:30 +0530

    update main.dart

--- Session 03/main.dart
+++ Session 03/main.dart
@@ -1,11 +1,31 @@
 void main() {
-  int age = 25;
-  String city = 'Matale';
-  bool isProfileLocked = false;
-  double weight = 60.5;
-  dynamic other = 'Other';
+  
+  String firstName = "Lahiru";
+  String lastName = "Bandara";
+  String email = "test@gmail.com";
+  String email2 = "test01@yahoo.com";
 
-  print(age);
-  print(city);
+  
+  print(firstName.toLowerCase());
+  print(lastName.toUpperCase());
+  // Contains
+  print(email.contains("gmail"));
+  print(email2.contains("gmail"));
+
+  print(email.endsWith("com"));
+
+  print(email.replaceAll('gmail', 'yahoo'));
+  print(email2.replaceAll('com', 'lk'));
+
+  print(email.split('.'));
+
+  // Trim
+  String fullName = "      Lahiru Dhananjaya Bandara     " ;
+
+  print(fullName);    // with whitespaces
+  print(fullName.trim()); // without whitespaces
+
+ 
+
   
 }
