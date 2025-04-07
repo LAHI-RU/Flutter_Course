bf30703d7cc81dd671bff007629045c13cba1a6f
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-04-07 21:53:09 +0530

    list

---
+++ Session 05/list.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name = "Kamal";
+
+  List<String> names = ["Kamal", "Nimal", "Saman", "Sunil", "Kumar"];
+
+  // List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  List<String> cities = ["Colombo", "Kandy", "Galle", "Matara", "Kurunegala"];
+
+  // print(names.length);
+
+  for (var name in names) {
+   print(name.toUpperCase());
+}
+}
+
+  
+    
+  
+    
--- Session 05/loops.dart
+++ Session 05/loops.dart
@@ -1,7 +1,20 @@
 void main() {
+  
+  /*
   int i=0;
-
+  print("statement 01");
   for(i; i<=10; i++){
     print("current value: $i"); //print(i)
+  }
+  print("statement 02");
+  */
+
+  // for loop 
+  for(int i=0; i<=100; i+=5){
+    print(i);
+  }
+  print('\n\n');
+  for(int j=100; j>=0; j-=5){
+    print(j);
   }
 }
