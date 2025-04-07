f0e507d74137057d7d4cca4410bce20b52bfd3e2
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-04-07 19:41:53 +0530

    if else if

---
+++ Session 05/01.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  String email = "user@gmail.com";
+  String password = "123456";
+  String mobile = "0772354619";
+
+  if(!email.contains("@gmail.com")) {
+    print("Please check your E-mail.");
+  } 
+  else if (password.length < 6) {
+    print("Password must be at least 6 characters long.");
+  }
+  else if (mobile.length != 10) {
+    print("Please check your mobile number.");
+  } 
+  else {
+    print("Account created successfully.");
+  }
+}
